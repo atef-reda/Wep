3c3343bbfbb42ddce46d70a99158e7daffd840c6
atef-reda <89020766+atef-reda@users.noreply.github.com>
2024-09-15 14:39:45 +0300

    lab day6 on javascript

--- Day4(JavaScript)/Labs/main.js
+++ Day4(JavaScript)/Labs/main.js
@@ -1,5 +1,5 @@
-// 2- Write a JavaScript program which swap the case of each character for string. 
-// //Example : 'Egypt' 
+// 2- Write a JavaScript program which swap the case of each character for string.
+// //Example : 'Egypt'
 // //Output : 'eGYPT'
 
 console.log("--------------------------------------");
@@ -10,8 +10,8 @@ str = str.replace(/[a-zA-Z]/g, (match) =>
 );
 console.log(str);
 
-// 3- Write a JavaScript code to extract unique characters from a string. 
-// //Example : "thequickbrownfoxjumpsoverthelazydog" 
+// 3- Write a JavaScript code to extract unique characters from a string.
+// //Example : "thequickbrownfoxjumpsoverthelazydog"
 // //Output : "thequickbrownfxjmpsvlazydg"
 console.log("--------------------------------------");
 
@@ -26,26 +26,23 @@ str = Array.from(uniqueChars).join("");
 
 console.log(str);
 
-
 // 4- Write a JavaScript code that finds the longest word within the string. (Bonus)
 // //Example : 'Web Development Tutorial'
 // //Output : 'Development'
 console.log("--------------------------------------");
 
-var str="Web Development Tutorial";
+var str = "Web Development Tutorial";
 console.log(str);
 
-words=str.split(' ');
-var longest_word="";
-var longest_length=0;
+words = str.split(" ");
+var longest_word = "";
+var longest_length = 0;
 
-for(word of words)
-{
-    if(word.length>longest_length)
-    {
-        longest_word=word;
-        longest_length=word.length;
-    }
+for (word of words) {
+  if (word.length > longest_length) {
+    longest_word = word;
+    longest_length = word.length;
+  }
 }
 console.log(longest_word);
 console.log("--------------------------------------");
